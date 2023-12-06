2cc6deec3e15015bb0856194f66bc060d2472c58
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2023-12-06 09:40:25 +0700

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_pertama/lessons/button.dart';
 import 'package:project_pertama/lessons/container.dart';
 import 'package:project_pertama/lessons/scaffold.dart';
 import 'package:project_pertama/lessons/text.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyText(),
+      home: const MyButton(),
     );
   }
 }
