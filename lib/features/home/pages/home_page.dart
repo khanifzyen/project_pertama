fe6b0bfa1058088b1feabcc5d48b20aa0df8556c
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2023-12-20 11:39:31 +0700

    add headline, category list

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project_pertama/features/home/widgets/category_home.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,17 +10,53 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         leading: IconButton(
           onPressed: () {},
-          icon: SvgPicture.asset('assets/icons/back.svg'),
+          icon: SvgPicture.asset('assets/icons/menu.svg'),
         ),
-        title: const Center(child: Text("Home")),
+        title: Center(
+            child: Text(
+          "Home",
+          style: GoogleFonts.poppins(
+            textStyle: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+                color: Color(0xff4A4543)),
+          ),
+        )),
         actions: [
           IconButton(
             onPressed: () {},
             icon: SvgPicture.asset('assets/icons/search.svg'),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 14.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 226,
+              child: Text(
+                'Discover the most modern furniture',
+                style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 22,
+                      color: Color(0xFF4A4543)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            const SizedBox(
+              height: 40,
+              child: CategoryHome(),
+            ),
+          ],
+        ),
       ),
     );
   }
